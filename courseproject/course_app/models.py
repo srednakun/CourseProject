31d23e6a3b10089f87fc747e12b8d68903d28f9a
Sredna <srednakun@gmail.com>
2020-05-03 16:06:07 -0500

    see all courses

--- courseproject/course_app/models.py
+++ courseproject/course_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    course_id = models.CharField(max_length = 10, primary_key = True)
+    course_title = models.CharField(max_length = 30)
+    course_description = models.CharField(max_length = 300)
+    course_credit = models.IntegerField()
+    course_instructor = models.CharField(max_length = 50)
+    course_startDate = models.DateField()
+
+    def _str_(self):
+        return self.course_title
--- courseproject/course_app/views.py
+++ courseproject/course_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from course_app.models import Course
 
 # Create your views here.
 def index(request):
-    name = {'name': "My name is Sredna Kunowski and I am an artist."}
-    
-    return render(request, 'course_app/index.html', context = name)
+    #test to print out my name
+    #name = {'name': "My name is Sredna Kunowski and I am an artist."}
+    #return render(request, 'course_app/index.html', context = name)
+
+    course_list = Course.objects.order_by('course_id')
+    course_dict = {'course': course_list}
+    return render(request, 'course_app/index.html', context = course_dict)
