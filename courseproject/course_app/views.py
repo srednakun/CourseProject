1d97e5971e5710424f652ec2eb918b03355053f0
Sredna <srednakun@gmail.com>
2020-05-04 15:38:09 -0500

    added course detail page

--- courseproject/course_app/views.py
+++ courseproject/course_app/views.py
@@ -11,3 +11,20 @@ def index(request):
     course_list = Course.objects.order_by('course_id')
     course_dict = {'course': course_list}
     return render(request, 'course_app/index.html', context = course_dict)
+
+#details view
+def details(request):
+
+	print("Request: " + str(request))
+	#Request: <WSGIRequest: GET '/details?pk=CSCI159'>
+	pk = request.GET.get('pk')
+	print("primark key = " + pk)
+
+	#search through the course list for a match
+	course_list = Course.objects.order_by('course_id')
+	for course in course_list:
+		if course.course_id == pk:
+			course_dict = {'course': course}
+
+	return render(request, 'course_app/details.html', context = course_dict)
+   
--- courseproject/courseproject/urls.py
+++ courseproject/courseproject/urls.py
@@ -20,4 +20,5 @@ from course_app import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('details', views.details, name = 'details')
 ]
